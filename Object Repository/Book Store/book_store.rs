6e5474d3a4a8eb8451537b9c212f0748b481789d
noveliave <novelia.vegananda@gmail.com>
2023-10-06 21:26:00 +0700

    commit_novel

---
+++ Object Repository/Book Store/book_store.rs
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>book_store</name>
+   <tag></tag>
+   <elementGuidId>bd1054c7-0d49-4e1e-af35-e93b3005aee3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[@id=&quot;app&quot;]/div/div/div[2]/div/div[6]/div/div[3]/h5</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//*[@id=&quot;app&quot;]/div/div/div[2]/div/div[6]/div/div[3]/h5</value>
+      <webElementGuid>d9fc7c81-6a33-4852-adde-ed6716861799</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>

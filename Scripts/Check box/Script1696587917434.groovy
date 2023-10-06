3136ce0874e5ce5d3638eb93491400dc0289dc8e
likapramaditya <49019378+likapramaditya@users.noreply.github.com>
2023-10-06 17:45:44 +0700

    first_commit

---
+++ Scripts/Check box/Script1696587917434.groovy
@@ -0,0 +1,40 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('https://demoqa.com/')
+
+WebUI.scrollToElement(findTestObject('Object Repository/card_element'), 0)
+
+WebUI.click(findTestObject('Object Repository/card_element'))
+
+WebUI.click(findTestObject('Object Repository/button_checkbox'))
+
+WebUI.scrollToElement(findTestObject('Object Repository/button_checkbox'), 0)
+
+WebUI.click(findTestObject('Object Repository/expand_home'))
+
+WebUI.click(findTestObject('Object Repository/expand_documents'))
+
+WebUI.click(findTestObject('Object Repository/expand_office'))
+
+WebUI.click(findTestObject('Object Repository/checkbox_classified'))
+
+WebUI.verifyTextPresent('classified', false)
+
+WebUI.closeBrowser()

ba9960a42fd970092457a923d1af6049d7699e83
Mayaastuti01 <maya.astuti1@gmail.com>
2023-10-06 16:15:11 +0700

    maya_BookStoreApp_profile

---
+++ Object Repository/logout_btn.rs
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>logout_btn</name>
+   <tag></tag>
+   <elementGuidId>04510a24-e815-4de4-863b-bfedb91c3026</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>//*[(text() = 'Log out' or . = 'Log out')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Log out</value>
+      <webElementGuid>fd250eed-4c1e-4c5f-9ea5-2b3c66eb15bf</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/demoqa_verify_login.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>demoqa_verify_login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>955ebf8d-f1b5-4037-a74c-03926f9cc5e4</testSuiteGuid>
+   <testCaseLink>
+      <guid>d4b30c81-cb83-4efa-8c69-e9139fa1409b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/demoqa</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
